f39bc5a507cf7280b64ebfcd79304882df0670cc
Max Lemieux <max.lemieux@gmail.com>
2020-04-22 12:44:42 -0700

    hard coded path to config.json to avoid import/no-dynamic-require error in linter

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
   });
-  // Creating a custom method for our User model. This will check if an unhashed password 
+  // Creating a custom method for our User model. This will check if an unhashed password
   // entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
