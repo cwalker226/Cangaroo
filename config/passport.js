37142407cd540cf9e9c8c412e9e7623f608db30e
Max Lemieux <max.lemieux@gmail.com>
2020-04-29 13:49:03 -0700

    removed debugging logs

--- config/passport.js
+++ config/passport.js
@@ -16,20 +16,17 @@ passport.use(new LocalStrategy(
     }).then((dbUser) => {
       // If there's no user with the given email
       if (!dbUser) {
-        console.log('passport returning bad email');
         return done(null, false, {
           message: 'Incorrect email.',
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
       if (!dbUser.validPassword(password)) {
-        console.log('passport returning bad password');
         return done(null, false, {
           message: 'Incorrect password.',
         });
       }
       // If none of the above, return the user
-      console.log('passport returning good user');
       return done(null, dbUser);
     });
   },
